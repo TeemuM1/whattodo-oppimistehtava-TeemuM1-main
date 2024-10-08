259ba630bd95e3878256158e7e5fbaceebcd4448
Teemu Matala <etema012@edu.xamk.fi>
2024-10-08 18:13:15 +0300

    Updated errorpage looks.

--- client/src/routes/ErrorPage.tsx
+++ client/src/routes/ErrorPage.tsx
@@ -1,7 +1,7 @@
-export default
-
-function ErrorPage() {
+export default function ErrorPage() {
     return(
-        <div className="Error">Error</div>
+        <div className="Error">
+            <h1>Page not found</h1>
+        </div>
     );
 }
